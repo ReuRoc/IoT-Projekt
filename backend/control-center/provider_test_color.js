d68f3c774611e5d35fced9d26f0793d10a9e129e
fatalchemist <fatalchemist420@gmail.com>
2019-06-05 13:24:10 +0200

    Add persistent runner to feed the mqtt broker

--- backend/control-center/provider_test_color.js
+++ backend/control-center/provider_test_color.js
@@ -3,8 +3,8 @@ const mqtt = require('async-mqtt');
 // const color_test = require('./color_test.js');
 const config = require('./config.json');
 const client = mqtt.connect(
-    config.mqtt_broker,
-    config.mqtt_options
+  config.mqtt_broker,
+  config.mqtt_options
 );
 const publishTestColors = async () => {
   console.log('starting to publish colors');
@@ -14,14 +14,29 @@ const publishTestColors = async () => {
       config.test_colors[getRandomInt(0, config.test_colors.length)],
       config.publishOptions
     );
-    await client.end();
   } catch (e) {
     console.log(e.stack);
     process.exit();
   }
 }
 
-client.on('connect', publishTestColors);
+let sendingMessages = true;
+const startSendingMessages = () => {
+  for (let i = 0, p = Promise.resolve(); sendingMessages == true; i++) {
+    p = p.then(_ => new Promise(resolve =>
+      setTimeout(function () {
+        publishTestColors();
+        resolve();
+      }, 1000)
+    ));
+  }
+}
+
+function stopSendingMessages() {
+  sendingMessages = false;
+}
+
+client.on('connect', startSendingMessages);
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
