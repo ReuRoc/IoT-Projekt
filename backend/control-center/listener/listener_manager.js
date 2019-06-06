9b86a55dd00bebe325abaf0f2ee9c19a841934ed
fatalchemist <fatalchemist420@gmail.com>
2019-06-06 10:04:23 +0200

    Add listener_manager

--- backend/control-center/listener/listener_manager.js
+++ backend/control-center/listener/listener_manager.js
@@ -1,24 +1,22 @@
-const util = require('util');
 const mqtt = require('async-mqtt');
 const config = require('../config.json');
 const client = mqtt.connect(
-    config.mqtt_broker,
-    config.mqtt_options
+  config.mqtt_broker,
+  config.mqtt_options
 );
 
-let listeningID = process.argv[3];
-let listeningTopic = `${config.mqtt_topic_main}${listeningID}/#/`;
+let listeningTopic = `${config.mqtt_topic_main}#/`;
 
 console.log('trying to connect to mqtt broker...');
 
 const startListening = async () => {
-    console.log('starting to listen...');
+  console.log('starting to listen...');
 }
 
 client.on('connect', startListening);
 
 client.on('message', function (topic, message) {
-    if (topic == this.) {
+  if (listeningTopic.substr(0, listeningTopic.length - 2).includes(topic)) {
 
-    }
+  }
 })
