71e8bbadd5937d15c92559e962b30ff6337857f8
bigalex <bigalex@gmx.de>
2019-06-06 22:27:11 +0200

    do some cleanup, make deviceid configurable

--- client/sketch_all/sketch_all.ino
+++ client/sketch_all/sketch_all.ino
@@ -1,5 +1,5 @@
 // based on example code 
-// (c) Michael Schoeffler 2017, http://www.mschoeffler.de
+// by (c) Michael Schoeffler 2017, http://www.mschoeffler.de
 
 #include "Wire.h" // This library allows you to communicate with I2C devices.
 
@@ -9,27 +9,30 @@
 #include <PubSubClient.h>
 #include <Adafruit_NeoPixel.h>
 
-#define LED_PIN  2
-#define LED_COUNT 5
-// Data wire for temp sensor is connected to GPIO15
-#define ONE_WIRE_BUS 4
+// this client ID also selects the channel for the mqtt messages
+#define CLIENT_ID "23"
+
+//DeviceAddress sensorAddress = { 0x28, 0xFF, 0x2F, 0x47, 0xA1, 0x16, 0x5, 0x9D };//sensor 1
+//DeviceAddress sensorAddress = { 0x28, 0xFF, 0x7A, 0xE3, 0xA0, 0x16, 0x4, 0x88 }; //sensor 2
+DeviceAddress sensorAddress = { 0x28, 0xFF, 0x24, 0xE, 0xA1, 0x16, 0x3, 0x43 };//sensor 3
+//DeviceAddress sensorAddress = { 0x28, 0xFF, 0x2B, 0x17, 0xA1, 0x16, 0x3, 0x87 };//sensor 4
 
 const char* ssid = "FI_WLAN";
 const char* password = "FI-Labor";
+
 
 // MQTT Broker IP address
 const char* mqtt_server = "10.43.0.77";
 
 // MQTT channels
-const char* mqtt_dir_chan = "devices/23/direction";
-const char* mqtt_temp_chan = "devices/23/temperature";
-const char* mqtt_state_chan = "devices/23/state";
+const char* mqtt_dir_chan = "devices/" CLIENT_ID "/direction";
+const char* mqtt_temp_chan = "devices/" CLIENT_ID "/temperature";
+const char* mqtt_state_chan = "devices/" CLIENT_ID "/state";
 
-//DeviceAddress sensor1 = { 0x28, 0xFF, 0x2F, 0x47, 0xA1, 0x16, 0x5, 0x9D };//sensor 1
-//DeviceAddress sensor1 = { 0x28, 0xFF, 0x7A, 0xE3, 0xA0, 0x16, 0x4, 0x88 }; //sensor 2
-DeviceAddress sensor1 = { 0x28, 0xFF, 0x24, 0xE, 0xA1, 0x16, 0x3, 0x43 };//sensor 3
-//DeviceAddress sensor1 = { 0x28, 0xFF, 0x2B, 0x17, 0xA1, 0x16, 0x3, 0x87 };//sensor 4
 
+#define LED_PIN  2     // LED data wire is on pin G2
+#define LED_COUNT 5    // there are 5 LEDs on most LED strands
+#define ONE_WIRE_BUS 4 // Data wire for temp sensor is connected to GPIO4
 
 // global variables
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -42,51 +45,19 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 //Change Temp Sensor Addresses
 
-long lastMsg = 0;
-char msg[50];
+
 
 const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
 
 int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
 int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
 int16_t temperature; // variables for temperature data
+long now=millis();
 int tangibledirection = 0;
 String statusColor = "";
 
-
-
-String translateEncryptionType(wifi_auth_mode_t encryptionType) {
-  switch (encryptionType) {
-    case (WIFI_AUTH_OPEN):
-      return "Open";
-    case (WIFI_AUTH_WEP):
-      return "WEP";
-    case (WIFI_AUTH_WPA_PSK):
-      return "WPA_PSK";
-    case (WIFI_AUTH_WPA2_PSK):
-      return "WPA2_PSK";
-    case (WIFI_AUTH_WPA_WPA2_PSK):
-      return "WPA_WPA2_PSK";
-    case (WIFI_AUTH_WPA2_ENTERPRISE):
-      return "WPA2_ENTERPRISE";
-  }
-}
-
-
-// helper function
-char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
-  char tmp_str[7]; // temporary variable used in convert function
-  sprintf(tmp_str, "%6d", i);
-  return tmp_str;
-}
-
-
-
-
-
 void initLed() {
   strip.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-                 // Turn OFF all pixels ASAP
   strip.show();               
   strip.setBrightness(50);
   strip.clear();
@@ -121,13 +92,15 @@ void initGyro() {
   Wire.write(0); // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
 }
+
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP8266Client")) {
-      Serial.println("connected");
+    if (client.connect("ESP8266Client" CLIENT_ID)) {
+      Serial.print("connected - ");
+      Serial.println("ESP8266Client" CLIENT_ID);
       // Subscribe
       client.subscribe(mqtt_state_chan);
     } else {
@@ -152,43 +125,9 @@ void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("+++++++++++");
   Serial.println();
   statusColor = messageTmp;
-  
-
-
 }
 
-void scanNetworks() {
- 
-  int numberOfNetworks = WiFi.scanNetworks();
- 
-  Serial.print("Number of networks found: ");
-  Serial.println(numberOfNetworks);
- 
-  for (int i = 0; i < numberOfNetworks; i++) {
- 
-    Serial.print("Network name: ");
-    Serial.println(WiFi.SSID(i));
- 
-    Serial.print("Signal strength: ");
-    Serial.println(WiFi.RSSI(i));
- 
-    Serial.print("MAC address: ");
-    Serial.println(WiFi.BSSIDstr(i));
- 
-    Serial.print("Encryption type: ");
-    String encryptionTypeDescription = translateEncryptionType(WiFi.encryptionType(i));
-    Serial.println(encryptionTypeDescription);
-    Serial.println("-----------------------");
- 
-  }
-}
- 
-
-
-
-
-void readGyro()
-{
+void readGyro() {
   Wire.beginTransmission(MPU_ADDR);
   Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
   Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
@@ -203,50 +142,33 @@ void readGyro()
   gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
   gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
   
-  // print out data
-  //Serial.print(" Acc Z-Axis = "); Serial.print(convert_int16_to_str(accelerometer_z));
-  //Serial.println();
-
-  //TODO: Lage bestimmen
-
+  //Lage bestimmen
   tangibledirection = 2; //default liegend
-  
   if (accelerometer_z > 5000) {
     tangibledirection = 0;
-    
   }
   if (accelerometer_z < -5000) {
     tangibledirection = 1;
-
   }
 
-
+  // Ausgeben und an MQTT-Broker senden
   char dirString[8];
   dtostrf(tangibledirection, 1, 0, dirString);
- Serial.print(" Direction = ");
- Serial.print(dirString);
- Serial.println();
- client.publish(mqtt_dir_chan, dirString);
-
-  
+  Serial.print(" Direction = ");
+  Serial.print(dirString);
+  Serial.println();
+  client.publish(mqtt_dir_chan, dirString);
 }
 
 void readTemp(void){
   //Serial.print("Requesting temperatures...");
   sensors.requestTemperatures(); // Send the command to get temperatures
-  //Serial.println("DONE");
-  Serial.print("Sensor 1(*C): ");
-  Serial.println(sensors.getTempC(sensor1)); 
   char tempString[8];
-  dtostrf(sensors.getTempC(sensor1), 1, 2, tempString);
- Serial.print(" Temp = ");
- Serial.print(tempString);
- Serial.println();
- client.publish(mqtt_temp_chan, tempString);
-
+  dtostrf(sensors.getTempC(sensorAddress), 1, 2, tempString);
+  Serial.print("Temperatursensor (*C): ");
+  Serial.println(tempString);
+  client.publish(mqtt_temp_chan, tempString);
 }
-long now=millis();
-
 
 void updateLeds(){
   strip.clear();
@@ -280,23 +202,24 @@ void updateLeds(){
     myColor =  strip.Color(255,255,255);
   }
   
-  strip.setPixelColor(0,strip.Color(0,0,0
-  ));
+  strip.setPixelColor(0,strip.Color(0,0,0));
   strip.setPixelColor(2,myColor);
   strip.setPixelColor(3,myColor);
   strip.setPixelColor(4,myColor);
-  strip.show();
-  
+  delay(1);      // see https://github.com/adafruit/Adafruit_NeoPixel/issues/139
+  //portDISABLE_INTERRUPTS();
+  strip.show();  // this does not really prevent flickering, but mitigates the problem a bit
+                 // this is a fundamental problem in the adafruit neopixel lib
+  //portENABLE_INTERRUPTS();
 }
 
 
 // ########################################
 void setup() {
-  Serial.begin(112500);
+  Serial.begin(115200);
   setup_wifi();
   initLed();
   initGyro();
-  //scanNetworks(); //scan for wifi networks
   sensors.begin();
   Serial.println(WiFi.macAddress());
   Serial.println(WiFi.localIP());
@@ -308,9 +231,11 @@ void setup() {
 
 // ########################################
 void loop() {
-  
+  if (client.connected()) {
+    client.loop(); //process incoming MQTT messages
+  }
+  updateLeds();
   if(millis()>=now+1000){
-    updateLeds();
     now = millis();
     if (!client.connected()) {
       reconnect();
@@ -318,6 +243,5 @@ void loop() {
     readGyro();
     readTemp();
   }
-    client.loop();
-    delay(10);
+  //delay(250);
 }
